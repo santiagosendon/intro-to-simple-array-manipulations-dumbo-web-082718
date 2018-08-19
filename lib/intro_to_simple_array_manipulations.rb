3afe5b71fbb8fcd982b477e450230b2ddbd28ef2
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-19 17:44:43 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
     next_country = "Niger"
-countries_in_western_africa.push("Niger")
+next_country.push("Niger")
  countries_in_western_africa.inspect
 end
