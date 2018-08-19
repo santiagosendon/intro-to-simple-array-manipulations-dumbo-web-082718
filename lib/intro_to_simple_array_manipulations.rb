ad403a5405c6698556dfaac6a3d49d2c2fdf3a3e
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-19 17:46:56 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def  using_push 
+def  using_push (countries_in_western_africa,next_country)
   do
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
     next_country = "Niger"
